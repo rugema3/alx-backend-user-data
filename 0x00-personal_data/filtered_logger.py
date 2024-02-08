6142017dc03578bb7d8d5c590a30b1494b68be7a
rugema3 <rugema61@gmail.com>
2024-02-08 20:07:16 +0200

    added docstrings

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Filtered_logger module."""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscate specific fields in a log message."""
     return re.sub('|'.join(map(re.escape, fields)), redaction, message)
