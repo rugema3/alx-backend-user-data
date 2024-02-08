e3f95033ce5b40e863160d6a2ce61393f63fd396
rugema3 <rugema61@gmail.com>
2024-02-08 19:41:51 +0000

    checking if the password is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,18 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against its hashed counterpart using bcrypt.
+
+    Args:
+    hashed_password (bytes): The hashed password to compare.
+    password (str): The password to validate.
+
+    Returns:
+    bool: True if the password matches the hashed password, False otherwise.
+    """
+    # Use bcrypt to validate the password
+    return bcrypt.checkpw(password.encode(), hashed_password)
