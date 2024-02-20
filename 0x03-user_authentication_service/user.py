fda81aeb171e879118c61f935e1fc7b9e6cfe8a4
rugema3 <rugema61@gmail.com>
2024-02-20 11:16:13 +0200

    modified the string to be of 250 characters

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ class User(Base):
     """A sqlalchemy class that contains a class mapping to the db table."""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
